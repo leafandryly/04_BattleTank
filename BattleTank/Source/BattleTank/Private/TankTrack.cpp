5d268c302782234acb762aeb9f332eb61ef08122
leafandryly <dylan3farley@gmail.com>
2019-01-02 11:34:29 -0500

    Physics Materials, Friction, Movement. Can't turn

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,6 +2,15 @@
 
 #include "TankTrack.h"
 
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%f: throttle: %f"), *Name, Throttle);
 
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+}
 
 

8828a2a08794215d2fc10c48355bdeb052b3b905
leafandryly <dylan3farley@gmail.com>
2019-01-02 19:14:00 -0500

    A better Component Architecture (Halfway)

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankMovementComponent.h"
+
+void UTankMovementComponent::IntendMoveForward(float Throw)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw %f"), Throw);
+
+	void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+	{
+		if (!LeftTrackToSet || !RightTrackToSet) { return; }
+		LeftTrack = LeftTrackToSet;
+		RightTrack = RightTrackToSet;
+	}
+}
+
+
